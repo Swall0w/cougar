bcb78a841408f887102110ff6a3cf3f2d5c5f7fb
Swall0w <technext.jpn@gmail.com>
2019-09-10 16:39:42 +0900

    [update]: linear assignment method from sklearn's one to scipy's.

--- cougar/graphs/models/tracking/deep_sort/linear_assignment.py
+++ cougar/graphs/models/tracking/deep_sort/linear_assignment.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 import numpy as np
-from sklearn.utils.linear_assignment_ import linear_assignment
+from scipy.optimize import linear_sum_assignment
 from cougar.graphs.models.tracking.deep_sort import kalman_filter
 
 
@@ -54,7 +54,8 @@ def min_cost_matching(
     cost_matrix = distance_metric(
         tracks, detections, track_indices, detection_indices)
     cost_matrix[cost_matrix > max_distance] = max_distance + 1e-5
-    indices = linear_assignment(cost_matrix)
+    row_idx, col_idx = linear_sum_assignment(cost_matrix)
+    indices = np.array(list(zip(row_idx, col_idx)))
 
     matches, unmatched_tracks, unmatched_detections = [], [], []
     for col, detection_idx in enumerate(detection_indices):
