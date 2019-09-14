6ec351be32a4798d00bbfa4fa11859e022fefee6
Swall0w <technext.jpn@gmail.com>
2019-09-14 21:15:51 +0900

    [update]: checkpoint.

--- cougar/common/__init__.py
+++ cougar/common/__init__.py
@@ -1,12 +1,13 @@
-from cougar.common.loads import read_config
+from cougar.common.loads import read_config, read_labels
 from cougar.common.dumps import dump_config
 from cougar.common.logger import setup_logger
 from cougar.common import comm
 from cougar.common.collect_env import collect_env_info
 from cougar.common import model_zoo
 from cougar.common import box_utils
+from cougar.common.checkpoint import CheckPointer
 
 
-__all__ = ['read_config', 'dump_config', 'setup_logger', 'comm', 'collect_env_info', 'model_zoo',
-           'box_utils',
+__all__ = ['read_config', 'read_labels', 'dump_config', 'setup_logger', 'comm', 'collect_env_info', 'model_zoo',
+           'box_utils', 'CheckPointer',
            ]
--- cougar/common/loads.py
+++ cougar/common/loads.py
@@ -5,3 +5,10 @@ def read_config(filename: str) -> dict:
     with open(filename, 'r') as f:
         ret = f.read()
     return hjson.loads(ret)
+
+
+def read_labels(filename: str) -> list:
+    with open(filename) as f:
+        lines = f.read().splitlines()
+
+    return lines
